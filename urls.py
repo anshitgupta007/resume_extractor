acf12366334966063baf6d1e623ae2f1ddcc628b
anshitgupta007 <anshitg365@gmail.com>
2024-04-26 21:18:26 +0530

    :

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from resume_extractor_app import views
-from  django.conf import settings
-from django.conf.urls.static import static
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.upload_zip, name='upload_zip'),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
